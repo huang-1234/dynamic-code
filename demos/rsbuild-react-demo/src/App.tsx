72e85873cbfa027158331f25d68e08602a29186a
huangsq <137707531@qq.com>
2025-07-15 09:18:57 +0800

    feat: init demo

---
+++ demos/rsbuild-react-demo/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Example from './tree/tree-diff/example';
+
+const App = () => {
+  return (
+    <div className="content">
+      <Example />
+    </div>
+  );
+};
+
+export default App;
